7c1ff7d57029914094eea0fb87c2fe2585244444
John Zhang <john.shimin.zhang@gmail.com>
2014-09-27 15:28:44 -0400

    add two sections of blogger app, form flow and basic article models

---
+++ blogger/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blogger::Application.config.secret_key_base = '38362d6a0fd2a46e3c15be7140ff8e646d314852bff9c6ad5c8ff99e9a9ccd9fba4d3a0e96126a629e31a5e2e7a05b328a013c0e350fc3f6fa6ab9b369b3e80a'
